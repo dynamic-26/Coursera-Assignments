1bc731a7276ea6032b6d6640eac77cf84a7ac552
Vivek Bhadola <57180959+dynamic-26@users.noreply.github.com>
2020-08-22 20:01:11 +0530

    Update script.js

--- mod_5_assig/js/script.js
+++ mod_5_assig/js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML(categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage =homeHtmlUrl;
-      homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName","'"+ chosenCategoryShortName"'");
+      homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName","'"+ chosenCategoryShortName+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
