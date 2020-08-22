b1f5aff19f9fa26d2fe16676c4db9d444b946d43
dynamic-26 <vivek.bhadola.mat19@itbhu.ac.in>
2020-08-22 20:15:18 +0530

    final change

--- mod_5_assig/js/script.js
+++ mod_5_assig/js/script.js
@@ -101,7 +101,8 @@ function buildAndShowHomeHTML(categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      var categoryChosen=chooseRandomCategory(categories);
+      var chosenCategoryShortName = categoryChosen.short_name;
       
 
 
@@ -116,8 +117,8 @@ function buildAndShowHomeHTML(categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage =homeHtmlUrl;
-      homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName","'"+ chosenCategoryShortName+"'");
+      
+      var homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlUrl,"randomCategoryShortName","'"+ chosenCategoryShortName+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
